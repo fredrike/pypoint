b5c9a701d2b7e24d796aa7f8c410360b61d8ec8a
Fredrik Erlandsson <fredrik.e@gmail.com>
2019-02-14 09:19:27 +0100

    version up

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pypoint",
-    version="1.0.8",
+    version="1.1.0",
     author="Fredrik Erlandsson",
     author_email="fredrik.e@gmail.com",
     description="API for Minut Point",
