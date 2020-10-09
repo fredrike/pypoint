a673d4b88669bbf050ba94b533762c104bb41ac5
Fredrik Erlandsson <fredrik.e@gmail.com>
2020-10-09 08:19:48 +0200

    Fix lint/formatting and make life easier

---
+++ Makefile
@@ -0,0 +1,35 @@
+.PHONY: default format white black lint test check clean pypireg pypi release
+
+default: check
+
+format: white
+	isort setup.py pypoint/*.py
+
+white: black
+
+black:
+	black . pypoint
+
+lint: requirements.txt setup.py
+	flake8
+
+check: format lint
+
+clean:
+	rm -f *.pyc
+	rm -rf .tox
+	rm -rf *.egg-info
+	rm -rf __pycache__ pypoint/__pycache__
+	rm -f pip-selfcheck.json
+	rm -rf pytype_output
+
+pypireg:
+	python setup.py register -r pypi
+
+pypi:
+	rm -f dist/*.tar.gz
+	python3 setup.py sdist
+	twine upload dist/*.tar.gz
+
+release:
+	git diff-index --quiet HEAD -- && make check && bumpversion patch && git push --tags && git push && make pypi
--- pypoint/__init__.py
+++ pypoint/__init__.py
@@ -1,9 +1,10 @@
 """Minut Point API."""
 
-import logging
 from datetime import timedelta
-from requests_oauthlib import OAuth2Session
+import logging
+
 from oauthlib.oauth2.rfc6749.errors import MissingTokenError
+from requests_oauthlib import OAuth2Session
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -18,46 +19,62 @@ MINUT_HOMES_URL = MINUT_API_URL + "/v1/homes"
 TIMEOUT = timedelta(seconds=10)
 
 EVENTS = {
-    'battery':  # On means low, Off means normal
-    ('battery_low', ''),
-    'button_press':  # On means the button was pressed, Off means normal
-    ('short_button_press', ''),
-    'cold':  # On means cold, Off means normal
-    ('temperature_low', 'temperature_risen_normal'),
-    'connectivity':  # On means connected, Off means disconnected
-    ('device_online', 'device_offline'),
-    'dry':  # On means too dry, Off means normal
-    ('humidity_low', 'humidity_risen_normal'),
-    'heat':  # On means hot, Off means normal
-    ('temperature_high', 'temperature_dropped_normal'),
-    'moisture':  # On means wet, Off means dry
-    ('humidity_high', 'humidity_dropped_normal'),
-    'sound':  # On means sound detected, Off means no sound (clear)
-    ('avg_sound_high', 'sound_level_dropped_normal'),
-    'tamper':  # On means the point was removed or attached
-    ('tamper', ''),
+    "battery": ("battery_low", ""),  # On means low, Off means normal
+    "button_press": (  # On means the button was pressed, Off means normal
+        "short_button_press",
+        "",
+    ),
+    "cold": (  # On means cold, Off means normal
+        "temperature_low",
+        "temperature_risen_normal",
+    ),
+    "connectivity": (  # On means connected, Off means disconnected
+        "device_online",
+        "device_offline",
+    ),
+    "dry": (  # On means too dry, Off means normal
+        "humidity_low",
+        "humidity_risen_normal",
+    ),
+    "heat": (  # On means hot, Off means normal
+        "temperature_high",
+        "temperature_dropped_normal",
+    ),
+    "moisture": (  # On means wet, Off means dry
+        "humidity_high",
+        "humidity_dropped_normal",
+    ),
+    "sound": (  # On means sound detected, Off means no sound (clear)
+        "avg_sound_high",
+        "sound_level_dropped_normal",
+    ),
+    "tamper": ("tamper", ""),  # On means the point was removed or attached
 }
 
 
 class PointSession(OAuth2Session):
     """Point Session class used by the devices."""
 
-    def __init__(self,
-                 client_id,
-                 client_secret=None,
-                 redirect_uri=None,
-                 auto_refresh_kwargs=None,
-                 token=None,
-                 token_saver=None):
+    def __init__(
+        self,
+        client_id,
+        client_secret=None,
+        redirect_uri=None,
+        auto_refresh_kwargs=None,
+        token=None,
+        token_saver=None,
+    ):
         """Initialize the Minut Point Session object."""
         from threading import RLock
+
         super().__init__(
             client_id,
             auto_refresh_url=MINUT_TOKEN_URL,
             redirect_uri=redirect_uri,
             auto_refresh_kwargs=auto_refresh_kwargs,
             token=token,
-            token_updater=token_saver)
+            token_updater=token_saver,
+        )
 
         self._client_id = client_id
         self._client_secret = client_secret
@@ -87,21 +104,26 @@ class PointSession(OAuth2Session):
             _LOGGER.debug("Token issues: %s", error)
         return self._token
 
-    def _request(self, url, request_type='GET', **params):
+    def _request(self, url, request_type="GET", **params):
         """Send a request to the Minut Point API."""
         try:
-            _LOGGER.debug('Request %s %s', url, params)
+            _LOGGER.debug("Request %s %s", url, params)
             response = self.request(
-                request_type, url, timeout=TIMEOUT.seconds, **params)
+                request_type, url, timeout=TIMEOUT.seconds, **params
+            )
             response.raise_for_status()
-            _LOGGER.debug('Response %s %s %.200s', response.status_code,
-                          response.headers['content-type'], response.json())
+            _LOGGER.debug(
+                "Response %s %s %.200s",
+                response.status_code,
+                response.headers["content-type"],
+                response.json(),
+            )
             response = response.json()
-            if 'error' in response:
-                raise OSError(response['error'])
+            if "error" in response:
+                raise OSError(response["error"])
             return response
         except OSError as error:
-            _LOGGER.warning('Failed request: %s', error)
+            _LOGGER.warning("Failed request: %s", error)
 
     def _request_devices(self, url, _type):
         """Request list of devices."""
@@ -111,11 +133,12 @@ class PointSession(OAuth2Session):
     def read_sensor(self, device_id, sensor_uri):
         """Return sensor value based on sensor_uri."""
         url = MINUT_DEVICES_URL + "/{device_id}/{sensor_uri}".format(
-            device_id=device_id, sensor_uri=sensor_uri)
-        res = self._request(url, request_type='GET', data={'limit': 1})
-        if not res or not res.get('values'):
+            device_id=device_id, sensor_uri=sensor_uri
+        )
+        res = self._request(url, request_type="GET", data={"limit": 1})
+        if not res or not res.get("values"):
             return None
-        return res.get('values')[-1].get('value')
+        return res.get("values")[-1].get("value")
 
     @property
     def is_authorized(self):
@@ -124,34 +147,33 @@ class PointSession(OAuth2Session):
 
     def user(self):
         """Update and returns the user data."""
-        return self._request(MINUT_USERS_URL + "/{}".format(self._token['user_id']))
+        return self._request(MINUT_USERS_URL + "/{}".format(self._token["user_id"]))
 
     def _register_webhook(self, webhook_url, events):
         """Register webhook."""
         response = self._request(
             MINUT_WEBHOOKS_URL,
-            request_type='POST',
+            request_type="POST",
             json={
-                'url': webhook_url,
-                'events': events,
+                "url": webhook_url,
+                "events": events,
             },
         )
         return response
 
     def remove_webhook(self):
         """Remove webhook."""
-        if self._webhook.get('hook_id'):
+        if self._webhook.get("hook_id"):
             self._request(
-                "{}/{}".format(MINUT_WEBHOOKS_URL, self._webhook['hook_id']),
-                request_type='DELETE',
+                "{}/{}".format(MINUT_WEBHOOKS_URL, self._webhook["hook_id"]),
+                request_type="DELETE",
             )
 
     def update_webhook(self, webhook_url, webhook_id, events=None):
         """Register webhook (if it doesn't exit)."""
-        hooks = self._request(MINUT_WEBHOOKS_URL, request_type='GET')['hooks']
+        hooks = self._request(MINUT_WEBHOOKS_URL, request_type="GET")["hooks"]
         try:
-            self._webhook = next(
-                hook for hook in hooks if hook['url'] == webhook_url)
+            self._webhook = next(hook for hook in hooks if hook["url"] == webhook_url)
             _LOGGER.debug("Webhook: %s", self._webhook)
         except StopIteration:  # Not found
             if events is None:
@@ -163,21 +185,18 @@ class PointSession(OAuth2Session):
     @property
     def webhook(self):
         """Return the webhook id and secret."""
-        return self._webhook['hook_id']
+        return self._webhook["hook_id"]
 
     def update(self):
         """Update all devices from server."""
         with self._lock:
-            devices = self._request_devices(MINUT_DEVICES_URL, 'devices')
+            devices = self._request_devices(MINUT_DEVICES_URL, "devices")
 
             if devices:
-                self._state = {
-                    device['device_id']: device
-                    for device in devices
-                }
+                self._state = {device["device_id"]: device for device in devices}
                 _LOGGER.debug("Found devices: %s", list(self._state.keys()))
                 # _LOGGER.debug("Device status: %s", devices)
-            homes = self._request_devices(MINUT_HOMES_URL, 'homes')
+            homes = self._request_devices(MINUT_HOMES_URL, "homes")
             if homes:
                 self._homes = homes
             return self.devices
@@ -186,25 +205,27 @@ class PointSession(OAuth2Session):
     def homes(self):
         """Return all known homes."""
         return {
-            home['home_id']: home
-            for home in self._homes if 'alarm_status' in home.keys()
+            home["home_id"]: home
+            for home in self._homes
+            if "alarm_status" in home.keys()
         }
 
     def _set_alarm(self, status, home_id):
         """Set alarm satus."""
         response = self._request(
             MINUT_HOMES_URL + "/{}".format(home_id),
-            request_type='PUT',
-            json={'alarm_status': status})
-        return response.get('alarm_status', '') == status
+            request_type="PUT",
+            json={"alarm_status": status},
+        )
+        return response.get("alarm_status", "") == status
 
     def alarm_arm(self, home_id):
         """Arm alarm."""
-        return self._set_alarm('on', home_id)
+        return self._set_alarm("on", home_id)
 
     def alarm_disarm(self, home_id):
         """Disarm alarm."""
-        return self._set_alarm('off', home_id)
+        return self._set_alarm("off", home_id)
 
     @property
     def devices(self):
@@ -214,7 +235,7 @@ class PointSession(OAuth2Session):
     def device(self, device_id):
         """Return a device object."""
         if len(device_id) == 1:
-            raise Exception('ERR FER')
+            raise Exception("ERR FER")
         return Device(self, device_id)
 
     @property
@@ -239,7 +260,7 @@ class Device:
 
     def __str__(self):
         """Representaion of device."""
-        return ('Device #{id} {name}').format(
+        return ("Device #{id} {name}").format(
             id=self.device_id,
             name=self.name or "",
         )
@@ -257,7 +278,7 @@ class Device:
     @property
     def ongoing_events(self):
         """Return ongoing events of device."""
-        return self.device['ongoing_events']
+        return self.device["ongoing_events"]
 
     @property
     def device_id(self):
@@ -267,38 +288,38 @@ class Device:
     @property
     def last_update(self):
         """Last update from device."""
-        return self.device['last_heard_from_at']
+        return self.device["last_heard_from_at"]
 
     @property
     def name(self):
         """Name of device."""
-        return self.device.get('description')
+        return self.device.get("description")
 
     @property
     def battery_level(self):
         """Battery level of device."""
-        return self.device['battery']['percent']
+        return self.device["battery"]["percent"]
 
     @property
     def device_info(self):
         """Info about device."""
         return {
-            'connections': {('mac', self.device['device_mac'])},
-            'identifieres': self.device['device_id'],
-            'manufacturer': 'Minut',
-            'model': 'Point v{}'.format(self.device['hardware_version']),
-            'name': self.device['description'],
-            'sw_version': self.device['firmware']['installed'],
+            "connections": {("mac", self.device["device_mac"])},
+            "identifieres": self.device["device_id"],
+            "manufacturer": "Minut",
+            "model": "Point v{}".format(self.device["hardware_version"]),
+            "name": self.device["description"],
+            "sw_version": self.device["firmware"]["installed"],
         }
 
     @property
     def device_status(self):
         """Status of device."""
         return {
-            'active': self.device['active'],
-            'offline': self.device['offline'],
-            'last_update': self.last_update,
-            'battery_level': self.battery_level,
+            "active": self.device["active"],
+            "offline": self.device["offline"],
+            "last_update": self.last_update,
+            "battery_level": self.battery_level,
         }
 
     @property
